a564a9d5065d9b6a15b3a632de08e11e98d9f377
Dailson Gomes <dailson.de.assis@gmail.com>
2020-04-14 13:13:09 -0300

    #1 - Create the project
    The project was created on Eclipse IDE

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author dailson
+ *
+ */
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
