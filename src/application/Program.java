7c92524c36d6425673e37681b2f3984b59f2ddd3
Dailson Gomes <dailson.de.assis@gmail.com>
2020-04-16 19:56:48 -0300

    #9 - little-improvement-in-board-printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,13 +14,12 @@ public class Program {
 	public static void main(String[] args) throws BoardException {
 
 		try {
-
 			ChessMatch chessMatch = new ChessMatch();
 			UI.printBoard(chessMatch.getPieces());
 
 		} catch (BoardException e) {
 			System.out.println("Error: " + e.getMessage());
-		
+
 		} catch (Exception e) {
 			System.out.println("Unexpected error");
 		}
