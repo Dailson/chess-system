78374bbf9e5ff6b7814d015e7f0788064382fb4f
Dailson Gomes <dailson.de.assis@gmail.com>
2020-04-20 20:10:02 -0300

    #13 - Possible moves of Rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,23 +5,31 @@ package chess;
 
 import board.Board;
 import board.Piece;
+import board.Position;
 
 /**
  * @author dailson
  *
  */
-public abstract class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece {
 	private Color color;
-	
-	
+
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-	
+
 	public Color getColor() {
 		return this.color;
 	}
 
+	/**
+	 * @param position
+	 * @return
+	 */
+	protected boolean isThereOppnentPice(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != this.color;
+	}
 
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -4,6 +4,7 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -11,7 +12,7 @@ import chess.Color;
  * @author dailson
  *
  */
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
 
 	/**
 	 * @param board
@@ -20,8 +21,7 @@ public class Rook extends ChessPiece{
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "R";
@@ -30,6 +30,58 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMovies() {
 		boolean[][] mat = new boolean[super.getBoard().getRows()][super.getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// Above
+		p.setValues(position.getRow() -1, position.getColumn());
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOppnentPice(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOppnentPice(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOppnentPice(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOppnentPice(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
+
 	}
 }
